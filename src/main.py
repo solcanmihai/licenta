477f10a6ea49522d3aa616a5abce5eaeb38e2645
Solcan Mihai <solcan.mihaiandrei@yahoo.com>
2021-04-02 12:08:07 +0300

    good results in training

--- src/main.py
+++ src/main.py
@@ -15,32 +15,33 @@ def keras_config():
 if __name__ == '__main__':
     keras_config()
     project_root_directory = os.getcwd()
-    trainer = TrainingService(14, 30, 10)
+    # trainer = TrainingService(14, 30, 10, project_root_directory)
+    #
+    # # trainer.read_raw_speaker_data('E:\\LICENTA\\SPEECHDATA\\wav', first_n_utterances=10, first_n_speakers=10, skip_n_speakers=0, file_to_save='hifi_10_10')
+    # trainer.read_precomputed_mfcc('hifi_40_200')
+    # # trainer.extract_mfcc_from_raw_data()
+    # trainer.train_model('lets_make_the_network_better_40_200')
 
-    trainer.read_raw_speaker_data('E:\\LICENTA\\SPEECHDATA\\wav', first_n_utterances=20, first_n_speakers=200, skip_n_speakers=0)
-    # trainer.extract_mfcc_from_raw_data()
-    trainer.train_model('optimized_reading_20_200')
+    trainer2 = TrainingService(14, 30, 10, project_root_directory)
+    trainer2.read_raw_speaker_data('E:\\LICENTA\\SPEECHDATA\\wav', first_n_utterances=4, first_n_speakers=10, skip_n_speakers=130)
+    trainer2.extract_mfcc_from_raw_data()
 
-    # trainer2 = TrainingService(14, 30, 10)
-    # trainer2.read_raw_speaker_data('E:\\LICENTA\\SPEECHDATA\\wav', first_n_utterances=4, first_n_speakers=10, skip_n_speakers=130)
-    # trainer2.extract_mfcc_from_raw_data()
-    #
-    # trainer3 = TrainingService(14, 30, 10)
-    # trainer3.read_raw_speaker_data('E:\\LICENTA\\SPEECHDATA\\wav', first_n_utterances=4, first_n_speakers=10, skip_n_speakers=130)
-    # trainer3.extract_mfcc_from_raw_data()
-    #
-    # enrollment = EnrollmentService("hifi_20_100", project_root_directory)
-    #
-    # for speaker in trainer2.speakers:
-    #     enrollment.compute_d_vector(speaker)
-    #
-    # for speaker in trainer3.speakers:
-    #     enrollment.compute_d_vector(speaker)
-    #
-    # for speaker1 in trainer2.speakers:
-    #     print('$$$$$$$$$$$$$$')
-    #     for speaker2 in trainer3.speakers:
-    #         if speaker1.name == speaker2.name:
-    #             for utterance in speaker2.utterances:
-    #                 print(f'{speaker1.name} {utterance.name}')
-    #                 print(EnrollmentService.compare_d_vectors(speaker1.d_vector, utterance.d_vector))
+    trainer3 = TrainingService(14, 30, 10, project_root_directory)
+    trainer3.read_raw_speaker_data('E:\\LICENTA\\SPEECHDATA\\wav', first_n_utterances=4, first_n_speakers=10, skip_n_speakers=130)
+    trainer3.extract_mfcc_from_raw_data()
+
+    enrollment = EnrollmentService("hifi_20_100", project_root_directory)
+
+    for speaker in trainer2.speakers:
+        enrollment.compute_d_vector(speaker)
+
+    for speaker in trainer3.speakers:
+        enrollment.compute_d_vector(speaker)
+
+    for speaker1 in trainer2.speakers:
+        print('$$$$$$$$$$$$$$')
+        for speaker2 in trainer3.speakers:
+            if speaker1.name == speaker2.name:
+                for utterance in speaker2.utterances:
+                    print(f'{speaker1.name} {utterance.name}')
+                    print(EnrollmentService.compare_d_vectors(speaker1.d_vector, utterance.d_vector))
--- src/service/Training.py
+++ src/service/Training.py
@@ -1,3 +1,4 @@
+import pickle
 from math import floor
 from typing import List
 
@@ -19,6 +20,7 @@ from sklearn.utils import shuffle
 from keras.utils import to_categorical
 import tensorflow as tf
 from scipy.spatial import distance
+from tensorflow.python.keras.metrics import categorical_accuracy
 
 from src.entities.Speaker import Utterance, Speaker
 
@@ -29,13 +31,14 @@ def check_if_hifi_microphone(utterance_name: str):
 
 class TrainingService:
 
-    def __init__(self, filterbank_nr: int, context_left_size: int, context_right_size: int):
+    def __init__(self, filterbank_nr: int, context_left_size: int, context_right_size: int, root_directory: str):
         self.filterbank_nr = filterbank_nr
         self.unique_speaker_number = 0
         self.speakers: List[Speaker] = []
         self.context_left_size = context_left_size
         self.context_right_size = context_right_size
         self.model = None
+        self.root_directory = root_directory
 
     def build_training_data(self):
         frames_with_context = []
@@ -53,9 +56,10 @@ class TrainingService:
         training_generator = DataGenerator(self.speakers)
 
         hidden_layers = Sequential([
-            Dense(256, activation='relu'),
+            Dense(512, activation='relu'),
+            Dense(512, activation='relu'),
             Dropout(0.2),
-            Dense(256, activation='relu'),
+            Dense(512, activation='relu'),
             Dropout(0.2),
             Dense(256, activation='relu'),
         ])
@@ -66,7 +70,7 @@ class TrainingService:
         training_model = Model(inputs=input_model, outputs=coefficient_extractor)
 
         training_output_layer = Dropout(0.3)(coefficient_extractor)
-        training_output_layer = Dense(self.unique_speaker_number, 'softmax')(training_output_layer)
+        training_output_layer = Dense(len(self.speakers), 'softmax')(training_output_layer)
 
         model = Model(inputs=input_model, outputs=training_output_layer)
 
@@ -78,13 +82,13 @@ class TrainingService:
 
         print(model.summary())
 
-        sgd = SGD(lr=0.02)
+        sgd = SGD(lr=0.008)
         # early_stopping = EarlyStopping(monitor='val_loss', patience=4)
-        # reduce_lr = ReduceLROnPlateau(monitor='val_loss', factor=0.5, patience=1, min_lr=0.00001)
+        # reduce_lr = ReduceLROnPlateau(monitor='loss', factor=0.5, patience=1, min_lr=0.001)
 
         csv_logger = CSVLogger('training.log')
         model.compile(optimizer=sgd, loss='categorical_crossentropy', metrics=['accuracy'])
-        model.fit(training_generator, epochs=1200, verbose=1, callbacks=[csv_logger])
+        model.fit(training_generator, epochs=512, verbose=1, callbacks=[csv_logger])
 
         os.chdir(os.path.join(os.path.abspath(os.path.dirname(__file__)), '..', '..', 'trained_models'))
         training_model.save(f'{save_name}.h5')
@@ -96,6 +100,7 @@ class TrainingService:
             first_n_utterances: int,
             first_n_speakers: int,
             skip_n_speakers: int,
+            file_to_save: str = 'delete_me',
     ):
         os.chdir(path_to_folder)
         speaker_names = os.listdir()
@@ -107,6 +112,8 @@ class TrainingService:
             utterances_names = list(filter(check_if_hifi_microphone, utterances_names))
 
             speaker = Speaker(speaker_name, speaker_number, [], None)
+
+            print(f'Reading speaker {speaker_name}...')
 
             for utterance_name in utterances_names[:first_n_utterances]:
                 path_to_file = os.path.join(os.getcwd(), speaker_name, utterance_name)
@@ -121,6 +128,16 @@ class TrainingService:
                 speaker.utterances.append(Utterance(utterance_name, None, frame_rate, mfcc_frames, None, None))
 
             self.speakers.append(speaker)
+
+        os.chdir(os.path.join(self.root_directory, '..', 'computed_mfcc'))
+        p_file = open(f'{file_to_save}.pkl', 'wb')
+        pickle.dump(self.speakers, p_file)
+        p_file.close()
+
+    def read_precomputed_mfcc(self, file_to_read: str):
+        os.chdir(os.path.join(self.root_directory, '..', 'computed_mfcc'))
+        p_file = open(f'{file_to_read}.pkl', 'rb')
+        self.speakers = pickle.load(p_file)
 
     def extract_mfcc_from_raw_data(self):
         for speaker in self.speakers:
@@ -163,11 +180,11 @@ class DataGenerator(keras.utils.Sequence):
         self.nr_mfcc = nr_mfcc
 
         self.list_speakers = list_speakers
-        self.total_frames_with_context = 0
+        # self.total_frames_with_context = 0
 
-        for speaker in self.list_speakers:
-            for utterance in speaker.utterances:
-                self.total_frames_with_context += len(utterance.mfcc_frames) / nr_mfcc - self.nr_context + 1
+        # for speaker in self.list_speakers:
+        #     for utterance in speaker.utterances:
+        #         self.total_frames_with_context += len(utterance.mfcc_frames) / nr_mfcc - self.nr_context + 1
         self.speaker = 0
         self.frame = context_left
         self.list_ids = []
@@ -180,6 +197,11 @@ class DataGenerator(keras.utils.Sequence):
                     slice_start = (frame_start_index - context_left) * nr_mfcc
                     slice_end = (frame_start_index + context_right + 1) * nr_mfcc
                     self.list_ids.append((speaker_index, utterance_index, slice_start, slice_end))
+
+        if len(self.list_ids) < batch_size:
+            self.batch_size = len(self.list_ids)
+
+        print(len(self.list_ids))
 
         self.on_epoch_end()
 
@@ -208,7 +230,7 @@ class DataGenerator(keras.utils.Sequence):
         self.indexes = shuffle(self.list_ids)
 
     def __data_generation(self, list_IDs_temp):
-        'Generates data containing batch_size samples' # X : (n_samples, *dim, n_channels)
+        'Generates data containing batch_size samples'
         # Initialization
         X = np.empty((self.batch_size, self.nr_context * self.nr_mfcc))
         y = np.empty((self.batch_size), dtype=int)
